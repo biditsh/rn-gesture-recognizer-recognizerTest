6163bcb3e4a5458a4a7a38b80a0e6cc3375083cc
Bidit Sharma <sharmabidit@gmail.com>
2018-12-02 19:16:06 -0600

    first commit, rn-gesture-recognizer package w/ working pad.js and trainer.js

---
+++ App.js
@@ -0,0 +1,25 @@
+import React, {Component} from 'react';
+import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Trainer} from 'rn-gesture-recognizer';
+
+export default class App extends Component{
+  render() {
+    return (
+      <View style={styles.container}>
+        <Trainer
+          path={'/Users/bidit/Projects/recognizerTest/'}
+          strokes={[]}
+          containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
+          color={'#000000'}
+          strokeWidth={4} 
+        />
+        </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  }
+});
