e5c75b4eff878d70c6123f25ffb28772cd9d11ba
Bidit Sharma <sharmabidit@gmail.com>
2018-12-05 10:51:17 -0600

    Snapshot commit: recognizer runs in App

--- App.js
+++ App.js
@@ -1,17 +1,22 @@
 import React, {Component} from 'react';
 import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Trainer} from 'rn-gesture-recognizer';
+import {Recognizer} from 'rn-gesture-recognizer';
+
+let gestureClassData = require('./gestureClasses.json'); 
 
 export default class App extends Component{
   render() {
     return (
       <View style={styles.container}>
-        <Trainer
-          path={'/Users/bidit/Projects/trainerTest/'}
+        <Recognizer
           strokes={[]}
           containerStyle={{backgroundColor: 'rgba(0,0,0,0.01)'}}
           color={'#000000'}
-          strokeWidth={4} 
+          strokeWidth={4}
+          // gestureClasses={gestureClassData}
+          trainingData={gestureClassData}
+          recognitionHandler = { (data) => {console.log(data)}}
+          // onChangeStrokes={(strokes) => console.log(strokes)}
         />
         </View>
     );
